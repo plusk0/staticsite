23decc81e74aafb6c0735817c3856a1b18a3b51b
plusk0 <pluskovich@outlook.com>
2025-06-05 16:06:02 +0200

    added image filtering. link filtering not tested (?)

--- src/codesplit.py
+++ src/codesplit.py
@@ -11,27 +11,27 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
             node_text = node.text.split(delimiter,2)
             new_nodes.extend([TextNode(node_text[0],TextType.TEXT),TextNode(node_text[1],text_type)])
             if len(node_text) == 3:
-                newnode3 = split_nodes_delimiter([TextNode(node_text[2],TextType.TEXT)], delimiter, text_type)
-                for i in newnode3:
+                more_nodes = split_nodes_delimiter([TextNode(node_text[2],TextType.TEXT)], delimiter, text_type)
+                for i in more_nodes:
                     new_nodes.append(i)
         else:
             new_nodes.append(node)
     return new_nodes
 
 def extract_markdown_images(text):                          
-    matches = re.findall(r"!\[.*?\)",text)                # Reference: https://regexr.com/
+    matches = re.findall(r"!\[.*?\)",text)              # Reference: https://regexr.com/
     tuples = []
 
     for match in matches:
         text = []
-        text = str(re.findall(r"\[.*?\]",match))          # Possible improvement: use a simpler implementation
-        text = re.sub(r"\[|\]|\(|\)|\'", "",text)         # for getting rid of ()[]'
-
+        text = str(re.findall(r"\[.*?\]",match))        # Possible improvement: use a simpler implementation
+        text = re.sub(r"\[|\]|\(|\)|\'", "",text)       # for getting rid of ()[]'
+                                                        # > using a list and removing first and last entry
         link = str(re.findall(r"\(.*?\)",match))
         link = re.sub(r"\[|\]|\(|\)|\'", "",link)
         image = (text, link)
         tuples.append(image)
-
+    print("tuples :",tuples)
     return tuples
 
 def extract_markdown_links(text):
@@ -48,3 +48,40 @@ def extract_markdown_links(text):
         tuples.append(webpage)
 
     return tuples
+
+def split_nodes_image(old_nodes):
+    new_nodes = []
+    for node in old_nodes:
+            if "!" in node.text:
+                node_text = []
+                node_text = node.text.split("!",1)
+                new_nodes.append(TextNode(node_text[0],TextType.TEXT))
+                node_text = node_text[1].split(")",1)
+
+                tuples = extract_markdown_images("!" + node_text[0] + ")")
+                print("test", tuples[0][0])
+                new_nodes.append(TextNode(tuples[0][0],TextType.IMAGE,tuples[0][1]))
+
+                if "!" in node_text[1]:
+                    more_nodes = split_nodes_image([TextNode(node_text[1],TextType.TEXT)])
+                    for i in more_nodes:
+                        new_nodes.append(i)
+    return new_nodes
+
+
+def split_nodes_link(old_nodes):
+    new_nodes = []
+    for node in old_nodes:
+            if "(" in node.text:
+                node_text = []
+                node_text = node.text.split("(",1)
+                new_nodes.extend(TextNode(node_text[0],TextType.TEXT))
+                node_text = node_text[1].split(")",1)
+                tuples = extract_markdown_links(node_text[0])
+                new_nodes.extend(TextNode(tuples[0],TextType.IMAGE,tuples[1]))
+
+                if "(" in node_text:
+                    more_nodes = split_nodes_link([TextNode(node_text[1],TextType.TEXT)])
+                    for i in more_nodes:
+                        new_nodes.append(i)
+    return new_nodes
--- src/test_converter.py
+++ src/test_converter.py
@@ -27,7 +27,23 @@ class TestConverter(unittest.TestCase):
         )
         self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
 
-
+    def test_split_images(self):
+        node = TextNode(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png)",
+            TextType.TEXT,
+        )
+        new_nodes = split_nodes_image([node])
+        self.assertListEqual(
+            [
+                TextNode("This is text with an ", TextType.TEXT),
+                TextNode("image", TextType.IMAGE, "https://i.imgur.com/zjjcJKZ.png"),
+                TextNode(" and another ", TextType.TEXT),
+                TextNode(
+                    "second image", TextType.IMAGE, "https://i.imgur.com/3elNhQu.png"
+                ),
+            ],
+            new_nodes,
+        )
 
 
 if __name__ == "__main__":
